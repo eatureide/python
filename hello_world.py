f8ab6bdd3ec59886fb5c1bd58028fdfbead01fdf
Eature <781746820@qq.com>
2021-12-12 16:30:05 +0800

    create

---
+++ hello_world.py
@@ -0,0 +1,113 @@
+# array = ['typescript', 'javascript', 'nodejs']
+# msg = f'her name is {array[1]}'
+# print(msg)
+# print(f'hi {array[0]}')
+# array.append('script')
+# print(array)
+
+# array.insert(1, 'haha')
+# print(array)
+
+# del array[1]
+# pop_item = array.pop()
+# print(pop_item)
+
+# array.remove('mike1')
+# print(array)
+
+# print(f'{array[1]} pigeon')
+# array.remove('javascript')
+# array.append('python')
+# print(array)
+# array.insert(0, 'java')
+# array.insert(1, 'c++')
+# array.append('go')
+# print(array)
+
+# print(f'only {array[1]}&{array[3]}')
+# print(f'fuck off{array.pop()}')
+# print(f'fuck off{array.pop()}')
+# print(f'{array} have a good night')
+
+# cars = ['bmw', 'audi', 'toyota', 'subaru']
+# cars.sort(reverse=True)
+# print(cars)
+
+# print(cars)
+# print(sorted(cars))
+# cars.reverse()
+# print(cars)
+
+# cities = ['shaoguan', 'guangzhou', 'wengyuan', 'xianggang', 'taiwan']
+# print(sorted(cities))
+# print(cities)
+# sorted(cities)
+# cities.reverse()
+# print(cities)
+# cities.sort()
+# print(cities)
+# cities.sort(reverse=True)
+# print(len(cities))
+# for item in cities:
+# print(item)
+
+# magicians = ['alice', 'david', 'carolina']
+# for magician in magicians:
+#     print(f'{magician.title()}, that was a great trick!')
+#     print(f'i cant wait to see your next trick {magician.title()}. \n')
+
+#     print('thank you everyone that was a great magic show!')
+
+# pizzas = ['pepperoni', 'chicken', 'beef']
+# for pizza in pizzas:
+#     print(f'i like {pizza} pizza')
+
+# print('i really love pizza')
+
+
+# animals = ['cat', 'dog', 'pig']
+# for animal in animals:
+#     print(f'a {animal} would like a great prt')
+
+# print('these animals would a great pet')
+
+# for value in range(5):
+#     print(value)
+
+# numbers = list(range(1, 11))
+# squares = []
+
+# for value in numbers:
+#     squares.append(value ** 2)
+
+# print(squares)
+# print(min(squares))
+# print(max(squares))
+# print(sum(squares))
+
+# squares = [value ** 2 for value in range(1, 11)]
+# print(squares)
+
+# numbers = list(range(21))
+# for number in numbers:
+#     print(number)
+
+# bigNumbers = list(range(1_000_000))
+# print(min(bigNumbers))
+# print(max(bigNumbers))
+# print(sum(bigNumbers))
+# print(list(range(1, 21, 3)))
+# numbers = list(range(3, 31, 3))
+# for number in numbers:
+#     print(number)
+
+# numberSquare = [value ** 2 for value in range(11)]
+# for number in numberSquare:
+#     print(number)
+# print(numberSquare)
+
+# numberList = list(range(10))
+# squares = []
+# for number in numberList:
+#     squares.append(number ** 2)
+# print(squares)
