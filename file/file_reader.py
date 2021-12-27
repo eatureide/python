8ac398eafa70aca4bd2dbe14033671e0f2c5b0ad
Eature <781746820@qq.com>
2021-12-27 19:01:22 +0800

    写入文件

--- file/file_reader.py
+++ file/file_reader.py
@@ -1,5 +1,5 @@
-with open('pi_million_digits.txt') as file_object:
-    lines = file_object.readlines()
+# with open('pi_million_digits.txt') as file_object:
+#     lines = file_object.readlines()
 
 # pi_string = ''
 # for line in lines:
@@ -8,3 +8,10 @@ with open('pi_million_digits.txt') as file_object:
 # print(pi_string[:52])
 
 
+# with open('learning_python.txt') as file_object:
+#     lines = file_object.readlines()
+
+# for line in lines:
+#     print(line.strip())
+
+# print(lines)
---
+++ file/white_message.py
@@ -0,0 +1,2 @@
+with open('programing.txt', 'w') as file_object:
+    file_object.write('i love programing')
